5a60d04be62a0a4f223798dc64f06c52bdb05cc7
ian-1 <iandavidwalton@mail.com>
2021-04-12 16:57:25 +0100

    add sinatra-contrib to gemfile and add secret, dog and cat responses

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
-get '/ do
-    "Hello World"
+get '/' do
+  'Hello World'
+end
+
+get '/secret' do
+  'The blue cow moos at 2pm'    
+end
+
+get '/dog' do
+  'The dog is called Rover'
+end
+
+get '/cat' do
+  'The cat is called Chairman Meow'
 end
