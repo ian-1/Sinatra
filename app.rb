4ed5ddc92620279da28243b9d37a50f7efa1fa59
ian-1 <iandavidwalton@mail.com>
2021-04-13 18:09:33 +0100

    add form to name cat

--- app.rb
+++ app.rb
@@ -13,7 +13,17 @@ get '/dog' do
   'The dog is called Rover'
 end
 
-get '/cat' do
-  @cat_name = ["Amigo", "Misty", "Almond"].sample
+get '/random_cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
   erb(:index)
+end
+
+post '/named_cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/cat_form' do
+  erb(:cat_form)
 end
